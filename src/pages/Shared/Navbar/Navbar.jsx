f4a1555dd724461314cc8971c417049fc00a85df
Talha <mdtalha2008@gmail.com>
2024-11-25 01:29:40 +0600

    navbar add

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,49 +1,90 @@
-import React from 'react';
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 
-const Navbar = () => {
-    return (
-        <div>
-            <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
+export const Navbar = () => {
+  const [dropDownState, setDropDownState] = useState(false);
+  const dropDownMenuRef = useRef();
+
+  useEffect(() => {
+    const closeDropDown = (e) => {
+      if (!dropDownMenuRef?.current?.contains(e?.target)) {
+        setDropDownState(false);
+      }
+    };
+
+    document.addEventListener("mousedown", closeDropDown);
+
+    return () => {
+      document.removeEventListener("mousedown", closeDropDown);
+    };
+  }, []);
+  return (
+    <div>
+      <nav className="flex items-center justify-between bg-[#393E46] px-4 py-2 text-white mb-24">
+        <div className="scale-100 cursor-pointer rounded-2xl px-3 py-2 text-xl font-semibold text-white transition-all duration-200 hover:scale-110">
+          <h2>Logo</h2>
         </div>
-    );
+        <ul className="hidden items-center justify-between gap-10 md:flex">
+          <li className="group flex  cursor-pointer flex-col">
+            Home
+            <span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
+          </li>
+          <li className="group flex  cursor-pointer flex-col">
+            Services
+            <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
+          </li>
+          <li className="group flex  cursor-pointer flex-col">
+            About
+            <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
+          </li>
+          <li className="group flex  cursor-pointer flex-col">
+            Contact
+            <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
+          </li>
+        </ul>
+        <div
+          ref={dropDownMenuRef}
+          onClick={() => setDropDownState(!dropDownState)}
+          className="relative flex transition-transform md:hidden"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="cursor-pointer"
+          >
+            {" "}
+            <line x1="4" x2="20" y1="12" y2="12" />{" "}
+            <line x1="4" x2="20" y1="6" y2="6" />
+            <line x1="4" x2="20" y1="18" y2="18" />{" "}
+          </svg>
+          {dropDownState && (
+            <ul className=" z-10  gap-2  bg-[#393E46]  absolute right-0 top-11 flex w-[200px] flex-col  rounded-lg   text-base ">
+              <li className="cursor-pointer  px-6 py-2 text-white rounded-t-lg hover:bg-sky-600 ">
+                Home
+              </li>
+              <li className="cursor-pointer  px-6 py-2 text-white hover:bg-sky-600 ">
+                Services
+              </li>
+              <li className="cursor-pointer  px-6 py-2 text-white hover:bg-sky-600 ">
+                About
+              </li>
+              <li className="cursor-pointer  px-6 py-2 text-white hover:bg-sky-600 ">
+                Contact
+              </li>
+            </ul>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
