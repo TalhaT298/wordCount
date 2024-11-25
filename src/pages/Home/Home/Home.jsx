1e3eb203be300bded9911a2cdc0463614395c927
Talha <mdtalha2008@gmail.com>
2024-11-25 20:58:30 +0600

    banner add at home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Word from '../Word/Word';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <Word></Word>
         </div>
     );
