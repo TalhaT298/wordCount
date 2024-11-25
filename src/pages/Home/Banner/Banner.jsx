d8c9c7b53170040175eedf57ce8bf2459c69202f
Talha <mdtalha2008@gmail.com>
2024-11-25 21:50:04 +0600

    banner add

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const Banner = () => {
-    return (
-        <div>
-            <div className="flex flex-col items-center justify-center py-20 bg-black text-white">
-            <h1 className="text-xl font-light" style={{ fontFamily: "Atma, serif", fontWeight: 600 }}>What is Countify?</h1>
-            <h2 className="text-5xl font-extrabold mt-4 mb-2 text-center" style={{ fontFamily: "Atma, sans-serif", fontWeight: 600 }}>
-                Get answers. Find inspiration. <br /> Be more productive.
-            </h2>
-            <p className="text-lg font-light mt-2 mb-8 text-center max-w-md">
-               বাংলা GPT এমন একটি প্ল্যাটফর্ম, যেখানে আপনি বাংলা প্রম্পটের মাধ্যমে সহজেই বাংলায় ChatGPT-এর ব্যবহার কার্যকরভাবে করতে পারবেন।
-            </p>
-            <div className="flex gap-4">
-                <button className="px-8 py-0 bg-white text-black rounded-full font-medium hover:bg-black hover:text-white">
-                ⤷ Start now
-                </button>
-                <button className="px-6 py-3  border-white rounded-full font-medium hover:bg-white hover:text-black">
-                    Download the app ➤
-                </button>
-            </div>
-        </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center py-20 bg-black text-white">
+        <h1
+          className="text-4xl font-light"
+          style={{ fontFamily: "Atma, serif", fontWeight: 600 }}
+        >
+          What is Countify?
+        </h1>
+
+        <p className="text-xl font-normal mt-2 mb-8 text-center mx-auto max-w-[50%] sm:max-w-[80%]">
+          Countify is a user-friendly platform where you can easily count words
+          and characters in seconds. Knowing the word count of a text can be
+          crucial in many situations. For instance, authors often need to meet
+          specific word requirements for articles, essays, reports, stories,
+          books, papers, and more. Countify ensures your content meets the
+          required length, whether you're aiming to hit a minimum word count or
+          stay within a maximum limit. It's the perfect tool to make your
+          writing process smoother and more efficient.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
