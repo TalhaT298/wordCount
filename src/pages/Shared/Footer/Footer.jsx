a84b5191e0e35c30e1b16eacc1b4f756058752e3
Talha <mdtalha2008@gmail.com>
2024-11-25 20:35:39 +0600

    countify add logo

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -14,85 +14,117 @@ import logo2 from "../../../assets/W.png";
 const Footer = () => {
   return (
     <footer className="footer  text-neutral-content items-center p-4">
-    <aside className="grid-flow-col items-center">
-    <div className="flex items-center">
-            <h2 className="text-xl" style={{ fontFamily: "Atma, serif", fontWeight: 600 }}>
-              Countify
-            </h2>
-            <img src={logo2} className="w-24 h-7" alt="Logo" style={{ marginLeft: "8px" }} />
-          </div>
-      <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
-    </aside>
-    <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                <Link to="https://www.facebook.com/oceancapp">
-                  <a href="https://www.facebook.com/oceancapp">
-                    <FaFacebookF href="https://www.facebook.com/oceancapp" size="1.2rem" color="white" />
-                  </a>
-                </Link>
-              </motion.div>
+      <aside className="grid-flow-col items-center">
+        <div className="flex items-center">
+          <h2
+            className="text-xl"
+            style={{ fontFamily: "Atma, serif", fontWeight: 600 }}
+          >
+            Countify
+          </h2>
+          <img
+            src={logo2}
+            className="w-24 h-7"
+            alt="Logo"
+            style={{ marginLeft: "8px" }}
+          />
+        </div>
+        <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+      </aside>
+      <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+          <Link to="https://www.facebook.com/oceancapp">
+            <a href="https://www.facebook.com/oceancapp">
+              <FaFacebookF
+                href="https://www.facebook.com/oceancapp"
+                size="1.2rem"
+                color="white"
+              />
+            </a>
+          </Link>
+        </motion.div>
 
-              <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                <Link to="https://www.facebook.com/oceancapp">
-                  <a href="https://www.facebook.com/oceancapp">
-                    <FaInstagram href="https://www.facebook.com/oceancapp" size="1.2rem" color="white" />
-                  </a>
-                </Link>
-              </motion.div>
+        <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+          <Link to="https://www.facebook.com/oceancapp">
+            <a href="https://www.facebook.com/oceancapp">
+              <FaInstagram
+                href="https://www.facebook.com/oceancapp"
+                size="1.2rem"
+                color="white"
+              />
+            </a>
+          </Link>
+        </motion.div>
 
-              <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                <Link to="https://www.linkedin.com/company/oceancapp/">
-                  <a href="https://www.linkedin.com/company/oceancapp/">
-                    <FaLinkedinIn href="https://www.linkedin.com/company/oceancapp/" size="1.2rem" color="white" />
-                  </a>
-                </Link>
-              </motion.div>
+        <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+          <Link to="https://www.linkedin.com/company/oceancapp/">
+            <a href="https://www.linkedin.com/company/oceancapp/">
+              <FaLinkedinIn
+                href="https://www.linkedin.com/company/oceancapp/"
+                size="1.2rem"
+                color="white"
+              />
+            </a>
+          </Link>
+        </motion.div>
 
-              <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                <Link to="https://x.com/ocean_capp">
-                  <a href="https://x.com/ocean_capp">
-                    <BsTwitterX href="https://x.com/ocean_capp" size="1.2rem" color="white" />
-                  </a>
-                </Link>
-              </motion.div>
-              <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                <Link to="https://wa.link/8bkntc">
-                  <a href="https://wa.link/8bkntc">
-                    <FaWhatsapp href="https://wa.link/8bkntc" size="1.2rem" color="white" />
-                  </a>
-                </Link>
-              </motion.div>
-              <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                <Link to="">
-                  <a href="">
-                    <FaDiscord size="1.2rem" color="white" />
-                  </a>
-                </Link>
-              </motion.div>
-              <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                <Link to="">
-                  <a href="">
-                    <FaThreads size="1.2rem" color="white" />
-                  </a>
-                </Link>
-              </motion.div>
-              <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                <Link to="">
-                  <a href="">
-                    <FaYoutube size="1.2rem" color="white" />
-                  </a>
-                </Link>
-              </motion.div>
-              <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
-                <Link to="mailto:oceancapp@gmail.com">
-                  <a href="mailto:oceancapp@gmail.com">
-                    <BiLogoGmail href="mailto:oceancapp@gmail.com" size="1.2rem" color="white" />
-                  </a>
-                </Link>
-              </motion.div>
-    </nav>
-  </footer>
-);
+        <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+          <Link to="https://x.com/ocean_capp">
+            <a href="https://x.com/ocean_capp">
+              <BsTwitterX
+                href="https://x.com/ocean_capp"
+                size="1.2rem"
+                color="white"
+              />
+            </a>
+          </Link>
+        </motion.div>
+        <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+          <Link to="https://wa.link/8bkntc">
+            <a href="https://wa.link/8bkntc">
+              <FaWhatsapp
+                href="https://wa.link/8bkntc"
+                size="1.2rem"
+                color="white"
+              />
+            </a>
+          </Link>
+        </motion.div>
+        <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+          <Link to="">
+            <a href="">
+              <FaDiscord size="1.2rem" color="white" />
+            </a>
+          </Link>
+        </motion.div>
+        <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+          <Link to="">
+            <a href="">
+              <FaThreads size="1.2rem" color="white" />
+            </a>
+          </Link>
+        </motion.div>
+        <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+          <Link to="">
+            <a href="">
+              <FaYoutube size="1.2rem" color="white" />
+            </a>
+          </Link>
+        </motion.div>
+        <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+          <Link to="mailto:oceancapp@gmail.com">
+            <a href="mailto:oceancapp@gmail.com">
+              <BiLogoGmail
+                href="mailto:oceancapp@gmail.com"
+                size="1.2rem"
+                color="white"
+              />
+            </a>
+          </Link>
+        </motion.div>
+      </nav>
+    </footer>
+  );
 };
 
 export default Footer;
